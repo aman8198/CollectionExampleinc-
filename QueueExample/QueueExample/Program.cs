e4bd7d4ef6606d2818ee9bc979654a311926983b
Aman kumar Jha <68696279+aman8198@users.noreply.github.com>
2024-06-24 21:44:54 +0530

    stacks and queue example

---
+++ QueueExample/QueueExample/Program.cs
@@ -0,0 +1,43 @@
+﻿//Queue Example
+//follows FIFO approach doesnt work on index
+//Enqueue and Dequeue
+
+namespace QueueExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create an object of Queue Class
+            Queue<string> queue = new Queue<string>();
+
+            //Enqueue (add)
+            queue.Enqueue("Task 3");
+            queue.Enqueue("Task 5");
+            queue.Enqueue("Task 1");
+            queue.Enqueue("Task 4");
+            queue.Enqueue("Task 2");
+
+            //foreach
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            //dequeue
+            queue.Dequeue();
+
+            //peek
+            queue.Peek();
+
+            //contains
+            queue.Contains("Task 1");
+
+            //To array
+            queue.ToArray();
+
+            //Clear
+            queue.Clear();
+        }
+    }
+}
---
+++ StackExample/StackExample/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace StackExample
+{
+    class Student
+    {
+        public int Marks { get; set; }
+        public int Rank { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //create object of stack
+            //stack follows LIFO
+            Stack<Student> student = new Stack<Student>();
+
+            //Add
+            student.Push(new Student() { Marks = 45 });
+            student.Push(new Student() { Marks = 61 });
+            student.Push(new Student() { Marks = 80 });
+
+            //foreach to print elements in stack
+            int r = 1;
+            foreach(Student item in student)
+            {
+                item.Rank = r;
+                Console.WriteLine(item.Marks + ", " + item.Rank);
+                r++;
+            }
+
+            //pop
+            student.Pop();
+
+            //peek
+            student.Peek();
+
+            //Count 
+            student.Count();
+
+            //Contains
+            student.Contains(new Student() { Marks = 45 });
+
+            //Convert to array
+            student.ToArray();
+
+            //Remove all elements
+            student.Clear();
+
+            //no index based element location
+            Console.ReadLine();
+        }
+    }
+}
